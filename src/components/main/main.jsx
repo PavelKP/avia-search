4f39a5ead4f146bbb6ed3e26d38f8e9efe915819
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-11-04 21:13:26 +0300

    Завершена вёрстка меню

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -10,39 +10,62 @@ const Main = () => {
             <label className="form__label">
               <input className="visually-hidden form__input-hidden" type="radio" name="sorting" value="priceToHigh" defaultChecked />
               <span className="form__input form__input--radio"></span>
-              <span className="form__text">- по возрастанию цены</span>
+              <span className="form__description">- по возрастанию цены</span>
             </label>
             <label className="form__label">
-              <input className="visually-hidden form__input-hidden" type="radio" name="sorting" value="priceToLow" defaultChecked />
+              <input className="visually-hidden form__input-hidden" type="radio" name="sorting" value="priceToLow" />
               <span className="form__input form__input--radio"></span>
-              <span className="form__text">- по убыванию цены</span>
+              <span className="form__description">- по убыванию цены</span>
             </label>
             <label className="form__label">
-              <input className="visually-hidden form__input-hidden" type="radio" name="sorting" value="time" defaultChecked />
+              <input className="visually-hidden form__input-hidden" type="radio" name="sorting" value="time" />
               <span className="form__input form__input--radio"></span>
-              <span className="form__text">- по времени в пути</span>
+              <span className="form__description">- по времени в пути</span>
             </label>
           </fieldset>
-          <fieldset className="form__set fieldset-reset">
+          <fieldset className="form__set form__set--filter fieldset-reset">
             <legend className="form__heading">Фильтровать</legend>
-            <label>
-              <input className="" type="checkbox" name="transfer-filter" value="ransfer-1" />
-            - 1 пересадка
-              <input className="" type="checkbox" name="transfer-filter" value="ransfer-0" />
-            - без пересадок
+            <label className="form__label">
+              <input className="visually-hidden form__input-hidden" type="checkbox" name="transfer-filter" value="transfer-1"/>
+              <span className="form__input form__input--box"></span>
+              <span className="form__description">- 1 пересадка</span>
+            </label>
+            <label className="form__label">
+              <input className="visually-hidden form__input-hidden" type="checkbox" name="transfer-filter" value="transfer-1"/>
+              <span className="form__input form__input--box"></span>
+              <span className="form__description">- без пересадок</span>
             </label>
           </fieldset>
-          <fieldset className="form__set fieldset-reset">
+          <fieldset className="form__set form__set--input fieldset-reset">
             <legend className="form__heading">Цена</legend>
-            От <input type="number" placeholder="0"/>
-            До <input type="number" placeholder="10000" />
+            <div className="form__description-input-wrapper">
+              <span className="form__description form__description--price">От</span>
+              <input className="form__input form__input--text" type="number" placeholder="0" inputMode="numeric"/>
+            </div>
+            <div className="form__description-input-wrapper">
+              <span className="form__description form__description--price">До</span>
+              <input className="form__input form__input--text" type="number" placeholder="10000" inputMode="numeric"/>
+            </div>
           </fieldset>
           <fieldset className="form__set fieldset-reset">
             <legend className="form__heading">Авиакомпания</legend>
-              <input className="" type="checkbox" name="company-filter" value="company-name" />
-            - <span>LOT Polish Airlines</span> от <span>21049</span> р.
-            <input className="" type="checkbox" name="company-filter" value="company-name" />
-            - <span>Аэрофлот - рос...</span> от <span>31733</span> р.
+
+            <label className="form__label">
+              <input className="visually-hidden form__input-hidden" type="checkbox" name="company-filter" value="company-name"/>
+              <span className="form__input form__input--box"></span>
+              <span className="form__description form__description--company">
+              -&nbsp;<span className="form__company">LOT Polish Airlines</span> от 
+                <span className="form__price">21049</span>&nbsp;р.
+              </span>
+            </label>
+            <label className="form__label">
+              <input className="visually-hidden form__input-hidden" type="checkbox" name="company-filter" value="company-name"/>
+              <span className="form__input form__input--box"></span>
+              <span className="form__description form__description--company">
+              -&nbsp;<span className="form__company">Аэрофлот - рос...</span> от 
+                <span className="form__price">31733</span>&nbsp;р.
+              </span>
+            </label>
           </fieldset>
         </form>
       </nav>

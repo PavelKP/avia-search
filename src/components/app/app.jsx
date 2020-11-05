30ff32d65bb9479135d6e9f689e55251b734a279
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-11-05 15:32:48 +0300

    Добавлена черновая подгрузка json

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import flights from '../../mocks/flights-mock';
 import Main from '../main/main';
 import Loading from '../loading/loading';
 
+const url = `https://pavelkp.github.io/json/flights.json`;
+
 const getFlights = () => {
-  return new Promise((resolve, rejects) => {
-    let s = flights.replace(/\\n/g, "\\n")
-      .replace(/\\'/g, "\\'")
-      .replace(/\\"/g, '\\"')
-      .replace(/\\&/g, "\\&")
-      .replace(/\\r/g, "\\r")
-      .replace(/\\t/g, "\\t")
-      .replace(/\\b/g, "\\b")
-      .replace(/\\f/g, "\\f");
-    // remove non-printable and other non-valid JSON chars
-    
-    s = s.replace(/[\u0000-\u001F]+/g, "");
-  
-    resolve(JSON.parse(s));
+  return fetch(url)
+  .then((response) => {
+    return response.json();
   })
-    .catch((err) => {
-      throw new Error(err)
-    });
 }
 
 class App extends React.Component {
@@ -36,9 +23,9 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-      getFlights()
+    getFlights()
       .then((flights) => this._flights = flights)
-      .then(()=> this.setState((state) => ({isLoading: !state.isLoading})))
+      .then(()=> this.setState((state) => ({isLoading: !state.isLoading})));
   }
 
   render() {

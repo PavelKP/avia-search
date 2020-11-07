4bdcf4a022cc2db6ebbff127c38cfad37ab19bb3
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-11-08 02:39:52 +0300

    Отрисовка карточек по нажатию WIP

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 import BoardItem from '../board-item/board-item';
 import {connect} from 'react-redux';
 
-const Board = ({flights}) => {
-  return (
-    <section className="board">
-      <ul className="flight list-reset">
-        {flights.slice(0, 2).map((flight, i)=> <BoardItem key={i} flight={flight}/>)}
-      </ul>
-      <button className="button button--more" type="button">Показать еще</button>
-    </section>
-  );
+class Board extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cards: 2,
+    }
+
+    this._handleLoadMoreClick = this._handleLoadMoreClick.bind(this);
+  }
+
+  _handleLoadMoreClick() {
+    this.setState((state) => ({cards: state.cards + 2}));
+  }
+
+  render() {
+    return (
+      <section className="board">
+        <ul className="flight list-reset">
+          {this.props.flights.slice(0, this.state.cards).map((flight, i) => <BoardItem key={i} flight={flight} />)}
+        </ul>
+        <button className="button button--more" type="button" onClick={this._handleLoadMoreClick}>Показать еще</button>
+      </section>
+    );
+  }
 }
 
 

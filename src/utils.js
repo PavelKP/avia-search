59b46d9d6705bac40ce6c3bbb3f9184d6e353c73
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-11-08 21:13:56 +0300

    Исправлен баг, с выодом времени

--- src/utils.js
+++ src/utils.js
@@ -7,11 +7,14 @@ export const extend = (a, b) => {
 
 export const formatDuration = (duration) => {
   const momentDuration = moment.duration(duration, `minutes`);
-  const durationHours = (momentDuration.hours() <= 0) ? `` : `${momentDuration.hours()} ч`;
+  const durationHours = (momentDuration.asHours() <= 0)
+    ? ``
+    : `${(momentDuration.asHours()).toString().match(/(\d+)/)[0]} ч`;
   const durationMinutes = (momentDuration.minutes() <= 0) ? `` : `${momentDuration.minutes()} мин`;
 
   return `${durationHours} ${durationMinutes}`;
 };
+
 
 
 export const formatDate = (date, format) => {
